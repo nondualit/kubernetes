058ee7c81451967ea0cdc05ef16b8bdee50c88a2
nondualit <anibal@nondualit.nl>
2019-06-05 13:42:51 +0200

    Update PersistentVolume_Claim.sh

--- PersistentVolume_Claim.sh
+++ PersistentVolume_Claim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ################################################################################
 # Author    : Anibal Ojeda
-# Version   : 0.1
+# Version   : 0.2
 # Date      : 5-6-2019
-# Description: Add and remove ersistentVolume and Claims from Kubernetes Cluster
+# Description: Add and remove PersistentVolume and Claims from Kubernetes Cluster
 ################################################################################
 #Placeholders
 volumedir=/opt/kube_conf_files/volumes/
@@ -17,14 +17,12 @@ case "$1" in
 
 -a)
 
-echo -n "Enter PersistentVolume name: "
+echo -n "Enter PersistentVolume and Claim name: "
 read var1
-echo -n "Enter PersistentVolumeClaim name: "
-read var2
 echo -n "Enter storage in Gb: "
-read var3
+read var2
 echo -n "Enter volume directory: "
-read var4
+read var3
 
 cat > $volumedir/$var1-pv-volume.yaml << EOF
 kind: PersistentVolume
@@ -36,29 +34,29 @@ metadata:
 spec:
   storageClassName: manual
   capacity:
-    storage: ${var3}Gi
+    storage: ${var2}Gi
   accessModes:
     - ReadWriteOnce
   hostPath:
-    path: "$var4"
+    path: "$var3"
 EOF
 
-cat > $volumedir/$var2-pv-claim.yaml << EOF
+cat > $volumedir/$var1-pv-claim.yaml << EOF
 kind: PersistentVolumeClaim
 apiVersion: v1
 metadata:
-  name: $var2-pv-claim
+  name: $var1-pv-claim
 spec:
   storageClassName: manual
   accessModes:
     - ReadWriteOnce
   resources:
     requests:
-      storage: ${var3}Gi
+      storage: ${var2}Gi
 EOF
 
 echo "- $var1-pv-volume.yaml" >> $kustom
-echo "- $var2-pv-claim.yaml" >> $kustom
+echo "- $var1-pv-claim.yaml" >> $kustom
 
 cd $volumedir
 kubectl apply -k ./
@@ -66,20 +64,18 @@ kubectl apply -k ./
 
 -d)
 
-echo -n "Enter PersistentVolume name: "
+echo -n "Enter PersistentVolume and Claim name: "
 read var1
-echo -n "Enter PersistentVolumeClaim name: "
-read var2
 echo -n "Enter volume directory: "
-read var3
+read var2
 
-    kubectl delete -f $volumedir/$var2-pv-claim.yaml
+    kubectl delete -f $volumedir/$var1-pv-claim.yaml
     kubectl delete -f $volumedir/$var1-pv-volume.yaml
     ex +g/$var1-pv-volume.yaml/d -cwq $kustom
-    ex +g/$var2-pv-claim.yaml/d -cwq $kustom
-    rm -rf /opt/$var3
+    ex +g/$var1-pv-claim.yaml/d -cwq $kustom
+    rm -rf /opt/$var2
     rm $volumedir/$var1-pv-volume.yaml
-    rm $volumedir/$var2-pv-claim.yaml
+    rm $volumedir/$var1-pv-claim.yaml
 
 ;;
 -h)
