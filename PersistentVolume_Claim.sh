26b51d5354e7edb297a00c34a4ffd002b9585309
nondualit <anibal@nondualit.nl>
2019-06-05 13:05:26 +0200

    Update PersistentVolume_Claim.sh

--- PersistentVolume_Claim.sh
+++ PersistentVolume_Claim.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 ################################################################################
 # Author    : Anibal Ojeda
-# Version   : 1.0
-# Date      :
-# Description:
+# Version   : 0.1
+# Date      : 5-6-2019
+# Description: Add and remove ersistentVolume and Claims from Kubernetes Cluster
 ################################################################################
+# Options
+while [ -n "$1" ]; do # while loop starts
+
+case "$1" in
+
+-a)
+
 echo -n "Enter PersistentVolume name: "
 read var1
 echo -n "Enter PersistentVolumeClaim name: "
@@ -50,3 +57,26 @@ echo "- $var2-pv-claim.yaml" >> /opt/kube_conf_files/volumes/kustomization.yml
 
 cd /opt/kube_conf_files/volumes/
 kubectl apply -k ./
+;;
+
+-d)
+
+echo -n "Enter PersistentVolume name: "
+read var1
+echo -n "Enter PersistentVolumeClaim name: "
+read var2
+echo -n "Enter volume directory: "
+read var3
+
+    kubectl delete -f /opt/kube_conf_files/volumes/$var2-pv-claim.yaml
+    kubectl delete -f /opt/kube_conf_files/volumes/$var1-pv-volume.yaml
+    ex +g/$var1-pv-volume.yaml/d -cwq /opt/kube_conf_files/volumes/kustomization.yml
+    ex +g/$var2-pv-claim.yaml/d -cwq /opt/kube_conf_files/volumes/kustomization.yml
+    rm -rf /opt/$var3
+    rm /opt/kube_conf_files/volumes/$var1-pv-volume.yaml
+    rm /opt/kube_conf_files/volumes/$var2-pv-claim.yaml
+  esac
+
+    shift
+
+done
